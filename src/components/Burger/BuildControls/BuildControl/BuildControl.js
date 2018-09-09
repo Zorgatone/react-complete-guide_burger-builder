4a3e6734638228699ce6fd46659f403d026cb164
Tommaso Ricci <zorgatone@gmail.com>
2018-09-09 15:41:26 +0200

    build controls working: add + remove

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,16 @@ import classes from "./BuildControl.css";
 const BuildControl = props => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button className={classes.Less}>Less</button>
-    <button className={classes.More}>More</button>
+    <button
+      disabled={props.disabled}
+      onClick={props.removed}
+      className={classes.Less}
+    >
+      Less
+    </button>
+    <button onClick={props.added} className={classes.More}>
+      More
+    </button>
   </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,13 @@ import BuildControls from "../../components/Burger/BuildControls";
 
 import Aux from "../../hoc/Aux";
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  bacon: 0.4,
+  cheese: 1.3,
+  meat: 0.7
+};
+
 class BurgerBuilder extends Component {
   constructor(props) {
     super(props);
@@ -15,15 +22,67 @@ class BurgerBuilder extends Component {
         bacon: 0,
         cheese: 0,
         meat: 0
-      }
+      },
+      totalPrice: 4
     };
+
+    this.addIngredientHandler = this.addIngredientHandler.bind(this);
+    this.removeIngredientHandler = this.removeIngredientHandler.bind(this);
+  }
+
+  addIngredientHandler(type) {
+    this.setState(prevState => {
+      const oldCount = prevState.ingredients[type];
+      const updatedCount = oldCount + 1;
+      const updatedIngredients = {
+        ...prevState.ingredients
+      };
+
+      updatedIngredients[type] = updatedCount;
+
+      const priceAddition = INGREDIENT_PRICES[type];
+      const oldPrice = prevState.totalPrice;
+      const newPrice = oldPrice + priceAddition;
+
+      return { ingredients: updatedIngredients, totalPrice: newPrice };
+    });
+  }
+
+  removeIngredientHandler(type) {
+    if (this.state.ingredients[type] > 0) {
+      this.setState(prevState => {
+        const oldCount = prevState.ingredients[type];
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+          ...prevState.ingredients
+        };
+
+        updatedIngredients[type] = updatedCount;
+
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = prevState.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+
+        return { ingredients: updatedIngredients, totalPrice: newPrice };
+      });
+    }
   }
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key of Object.keys(disabledInfo)) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
+        />
       </Aux>
     );
   }
