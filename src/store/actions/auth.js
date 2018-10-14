da3114a8a36eca78d072d8494810dd469f9ebb0e
Tommaso Ricci <zorgatone@gmail.com>
2018-10-14 12:48:03 +0200

    Added logout on time expiration

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -17,6 +17,16 @@ export const authFail = error => ({
   error: error
 });
 
+export const logout = () => ({
+  type: actionTypes.AUTH_LOGOUT
+});
+
+export const checkAuthTimeout = expirationTime => dispatch => {
+  setTimeout(() => {
+    dispatch(logout());
+  }, expirationTime * 1000);
+};
+
 export const auth = (email, password, isSignup = true) => dispatch => {
   dispatch(authStart());
 
@@ -37,6 +47,7 @@ export const auth = (email, password, isSignup = true) => dispatch => {
     .post(url, authData)
     .then(response => {
       dispatch(authSuccess(response.data.idToken, response.data.localId));
+      dispatch(checkAuthTimeout(response.data.expiresIn));
     })
     .catch(err => {
       dispatch(authFail(err.response.data.error));
