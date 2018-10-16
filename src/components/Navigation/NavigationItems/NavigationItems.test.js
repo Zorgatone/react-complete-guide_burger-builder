31efc86aa83a9c567356c4ebff3b6110cecf4dae
Tommaso Ricci <zorgatone@gmail.com>
2018-10-16 23:02:04 +0200

    Check specific component is contained

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,4 +23,12 @@ describe("<NavigationItems />", () => {
 
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it("should render a logout <NavigationItem /> element if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
 });
